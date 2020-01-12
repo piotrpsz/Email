83d26561ab74e7c67205873c2f350361e540560c
piotr <piotr@beesoft.pl>
2020-01-12 17:43:51 +0100

    nc

--- main.go
+++ main.go
@@ -18,34 +18,29 @@ func main() {
 		return
 	}
 
-	p3 := pop3.New(addr, -1, pop3.TLS)
-	if p3 == nil {
-		return
-	}
-	defer func() {
-		if q := p3.Quit(); q != nil {
-			fmt.Println(q.Data)
-		}
-		p3.Close()
-	}()
+	if p3 := pop3.New(addr, -1, pop3.TLS); p3 != nil {
+		defer func() {
+			if q := p3.Quit(); q != nil {
+				fmt.Println(q.Data)
+			}
+			p3.Close()
+		}()
 
-	rsp := p3.Read()
-	if rsp.IsOK() {
-		fmt.Println(rsp.Data)
+		if rsp := p3.Read(); rsp != nil && rsp.IsOK() {
+			fmt.Println(rsp.Data)
 
-		if _, ok := p3.Auth(user, pass); ok {
-			if r, ok := p3.Stat(); ok {
-				fmt.Println("STAT:", r.Data)
+			if _, ok := p3.Auth(user, pass); ok {
+				if r, ok := p3.Stat(); ok {
+					fmt.Println("STAT:", r.Data)
 
-				if items := p3.List(); len(items) > 0 {
-					for _, item := range items {
-						p3.ReadEmail(item)
-						fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+					if items := p3.List(); len(items) > 0 {
+						for _, item := range items {
+							p3.ReadEmail(item)
+							fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+						}
 					}
-
 				}
 			}
 		}
-
 	}
 }
